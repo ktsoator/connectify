01b8f1e89a7ae573cc6a202636a127ad560c7727
ktsoator <ktsoator@gmail.com>
2026-02-05 22:13:57 +0800

    refactor: simplify main by encapsulating initializations

---
+++ internal/web/init.go
@@ -0,0 +1,26 @@
+package web
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() *gin.Engine {
+	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		// AllowOriginFunc: func(origin string) bool {
+		// 	return origin == "https://github.com"
+		// },
+		MaxAge: 12 * time.Hour,
+	}))
+
+	return server
+}
--- main.go
+++ main.go
@@ -1,37 +1,25 @@
 package main
 
 import (
-	"time"
-
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/ktsoator/connectify/internal/repository"
 	"github.com/ktsoator/connectify/internal/repository/dao"
 	"github.com/ktsoator/connectify/internal/service"
 	"github.com/ktsoator/connectify/internal/web"
+	"gorm.io/gorm"
 )
 
 func main() {
-	router := gin.Default()
-
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		// AllowOriginFunc: func(origin string) bool {
-		// 	return origin == "https://github.com"
-		// },
-		MaxAge: 12 * time.Hour,
-	}))
-
+	router := web.InitRouter()
 	db := dao.InitDB()
+	initUser(db, router)
+	router.Run(":8080")
+}
+
+func initUser(db *gorm.DB, router *gin.Engine) {
 	userDAO := dao.NewUserDAO(db)
 	userRepo := repository.NewUserRepository(userDAO)
 	userService := service.NewUserService(userRepo)
 	userHandler := web.NewUserHandler(userService)
 	userHandler.RegisterRoutes(router)
-
-	router.Run(":8080")
 }
